d01b21eafa2bf47560d543df4ee66e7d150f1d68
camilakyj <yagimaj@gmail.com>
2026-02-25 19:43:38 -0300

    Add files via upload

---
+++ Exercício 2 Aula 2.java
@@ -0,0 +1,12 @@
+// Camila Aki Exercício 2 Aula 2
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+    System.out.println("Minha idade:" + 18 + "anos");
+    System.out.println("Minha altura:" + 1.54 + "m");
+    System.out.println("Meu status na aula"+ "presente");
+    
+    }
+}
