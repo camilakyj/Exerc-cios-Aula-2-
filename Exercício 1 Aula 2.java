b909f27c5029bfb06f03b342890c051bcc375742
camilakyj <yagimaj@gmail.com>
2026-02-25 19:36:27 -0300

    Add files via upload

---
+++ Exercício 1 Aula 2.java
@@ -0,0 +1,12 @@
+// Camila Aki exercício 1 
+
+public class Main {
+    
+    public static void main(String[] args) {
+    System.out.println("Nome Completo: Camila Aki Yagima Jeronimo");
+    System.out.println("Registro Academico: 1143441554");
+    System.out.println("Curso: Analise e Desenvolvimento de Sistemas"); 
+    
+    }
+
+}
