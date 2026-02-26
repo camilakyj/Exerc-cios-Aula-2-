7ad21da3410eb0ccb9bced30bf8349cb2c04cfd7
camilakyj <yagimaj@gmail.com>
2026-02-26 19:24:35 -0300

    Add files via upload

---
+++ Exercício 1 - Tabuada.java
@@ -0,0 +1,11 @@
+// Exercicio 1. Tabuada 
+
+public class  Exercicio1 {
+    public static void main(Sring[] args) {
+        int numero = 5; for (int i=1; i<=10; i++){ System.out.printf("%d x %d= %d%n",numero,i,numero*i);
+        
+
+        }
+    }
+} 
+    
