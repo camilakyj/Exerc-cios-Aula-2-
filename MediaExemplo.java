88c2ec9ccba0c3b77a994552d180cf75e85b1f6d
camilakyj <yagimaj@gmail.com>
2026-02-27 18:03:09 -0300

    Exercicio 5 aula 2

---
+++ MediaExemplo.java
@@ -0,0 +1,19 @@
+package dobackofront;
+import java.util.Scanner;
+public class MediaExemplo {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite a primeira nota");
+        double nota1 = entrada.nextDouble();
+        System.out.println("Digite a segunda nota");
+        double nota2 = entrada.nextDouble();
+        System.out.println("Digite a terceira nota");
+        double nota3 = entrada.nextDouble();
+        double media = (nota1 + nota2 + nota3) / 3;
+        System.out.println("Media das notas" + media);
+        entrada.close();
+
+
+    }
+    }
+
