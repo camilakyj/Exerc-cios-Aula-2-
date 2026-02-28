0c1eeb25947f1017b018282d21134d3ce0f860ce
camilakyj <yagimaj@gmail.com>
2026-02-28 15:51:36 -0300

    Add files via upload

---
+++ Exercicio6b.java
@@ -0,0 +1,10 @@
+package dobackofront;
+// Exercicio 6b Camila Aki
+public class Exercicio6b {
+    static void main(String[] args) {
+        double resultado= Math.pow(2,(5.0/20.0))+30.0/(15*15);
+        System.out.println(resultado);
+
+
+    }
+}
