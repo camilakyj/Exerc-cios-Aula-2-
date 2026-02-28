5d173945e73b9e2501a18eb7021afc27128a9945
camilakyj <yagimaj@gmail.com>
2026-02-28 15:28:00 -0300

    Add files via upload

---
+++ Exercicio6.java
@@ -0,0 +1,12 @@
+package dobackofront;
+// Exercicio 6 Camila Aki
+
+public class Exercicio6 {
+    static void main(String[] args) {
+        double resultado= (20-15)/2;
+        System.out.println("O resultado da expressão é"+ resultado);
+
+
+
+    }
+}
