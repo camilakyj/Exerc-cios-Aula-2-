3b7c1b4b0c03b9402f40f9446945ec3f3a5eab01
camilakyj <yagimaj@gmail.com>
2026-02-28 16:17:29 -0300

    Add files via upload

---
+++ Exercicio6d.java
@@ -0,0 +1,11 @@
+package dobackofront;
+// Exercicio6d Camila Aki
+public class Exercicio6d {
+    static void main(String[] args) {
+        int resultado= 23%4;
+        System.out.println(resultado);//3
+
+
+        }
+    }
+
