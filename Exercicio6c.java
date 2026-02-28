acd3585176611bae4083ef7ef2bf650faf2a258b
camilakyj <yagimaj@gmail.com>
2026-02-28 16:04:16 -0300

    Add files via upload

---
+++ Exercicio6c.java
@@ -0,0 +1,10 @@
+package dobackofront;
+// Exercicio 6c Camila Aki
+public class Exercicio6c {
+    static void main(String [] args) {
+        double resultado= 35.0 /(6+2);
+        System.out.println("O resultado da expressão é:" + resultado);
+
+
+    }
+}
