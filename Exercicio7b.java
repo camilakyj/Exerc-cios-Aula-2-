9560300cfc7aeb377ba572470bd65c764656c227
camilakyj <yagimaj@gmail.com>
2026-03-01 17:51:31 -0300

    Add files via upload

---
+++ Exercicio7b.java
@@ -0,0 +1,10 @@
+package dobackofront;
+// Exercicio 7 b
+public class Exercicio7b {
+    static void main(String[] args) {
+        boolean resultado= !((20%4/2)==1||(9!=9));
+        System.out.println(resultado);
+
+
+    }
+}
