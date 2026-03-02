38e84ab583e67c137983494256454a4ac56ce312
camilakyj <yagimaj@gmail.com>
2026-03-02 16:03:34 -0300

    Add files via upload

---
+++ Exercicio8.java
@@ -0,0 +1,21 @@
+package dobackofront;
+
+import java.util.Scanner;
+
+// Exercício 8 Camila Aki
+
+public class Exercicio8 {
+public static void main(String[] args) {
+    Scanner entrada= new Scanner(System.in);
+    double nota1= 8.0;
+    double nota2= 7.5;
+    double nota3= 4.5;
+    double nota4= 9;
+    double media= (nota1+nota2+nota3+nota4)/4;
+    System.out.println("Media das notas é:"+media);
+    entrada.close();
+
+
+        }
+    }
+
