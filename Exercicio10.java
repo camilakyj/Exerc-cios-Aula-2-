3fd4afd1216d4144b10e32426209674e8a588c3c
camilakyj <yagimaj@gmail.com>
2026-03-02 16:53:29 -0300

    Add files via upload

---
+++ Exercicio10.java
@@ -0,0 +1,15 @@
+package dobackofront;
+import java.util.Scanner;
+
+public class Exercicio10 {
+public static void main(String[] args) {
+    Scanner entrada=new Scanner(System.in);
+    double raio=5;
+    double pi = 3.14159;
+    double area = pi*(raio*raio);
+    System.out.println("A area do circulo é:"+area+ "cm \n");
+    entrada.close();
+
+
+    }
+}
