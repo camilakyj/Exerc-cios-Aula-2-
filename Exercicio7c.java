e89e3fec52a7edfd28a16f65e8b479e420987b4c
camilakyj <yagimaj@gmail.com>
2026-03-02 15:45:40 -0300

    Add files via upload

---
+++ Exercicio7c.java
@@ -0,0 +1,12 @@
+package dobackofront;
+// Exercicio 7b Camila Aki
+public class Exercicio7c {
+    public static void main(String[] args) {
+        boolean resultado = (5 % 2) > 3;
+        System.out.println(resultado);
+    }} 
+
+
+
+
+
