8478f05b2b0712a0300db0e44e5b15a4a45b05d9
camilakyj <yagimaj@gmail.com>
2026-03-02 16:33:26 -0300

    Add files via upload

---
+++ Exercicio9.java
@@ -0,0 +1,15 @@
+package dobackofront;
+import java.util.Scanner;
+
+public class Exercicio9 {
+    public static void main(String[] args) {
+    Scanner entrada=new Scanner(System.in);
+    int lado = 350;
+    int area= lado*lado;
+    System.out.println("A area do quadrado é:"+ area+ "m \n");
+    entrada.close();
+
+
+        }
+    }
+
