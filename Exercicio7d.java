c69e5647ebcefc48df30be344b54c41a0d98eff1
camilakyj <yagimaj@gmail.com>
2026-03-03 18:43:49 -0300

    Add files via upload

---
+++ Exercicio7d.java
@@ -0,0 +1,19 @@
+package dobackofront;
+import java.util.Scanner;
+// Exercicio 7d Camila Aki 
+public class Exercicio7d {
+    static void main(String[] args) {
+        Scanner entrada= new Scanner(System.in);
+
+                String a = "a";
+                String A = "A";
+
+                System.out.println(a == A);
+                entrada.close();
+
+            }
+        }
+
+
+
+
